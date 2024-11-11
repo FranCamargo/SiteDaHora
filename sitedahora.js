328517e0bee2432e66f9d04cd1804ead7e9822e0
Fran Camargo <francamargoads@gmail.com>
2024-11-11 18:09:33 -0300

    ajuste

--- sitedahora.js
+++ sitedahora.js
@@ -4,7 +4,7 @@ function carregar() {
   var data = new Date();
   var hora = data.getHours();
 
-  msg.innerHTML = `Agora são ${hora} horas`;
+  msg.innerHTML = `Agora são ${hora} horas.`;
 
   if ((hora >= 0) && (hora < 12)) {
     // Bom dia!
