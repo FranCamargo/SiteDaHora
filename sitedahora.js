95a3d2106b656b5a3498d85f91c01e0bfe3e1c30
Fran Camargo <francamargoads@gmail.com>
2024-11-11 17:47:56 -0300

    inicial

---
+++ sitedahora.js
@@ -0,0 +1,22 @@
+function carregar() {
+  var msg = window.document.getElementById('msg');
+  var img = window.document.getElementById('imagem');
+  var data = new Date();
+  var hora = data.getHours();
+
+  msg.innerHTML = `Agora são ${hora} horas`;
+
+  if ((hora >= 0) && (hora < 12)) {
+    // Bom dia!
+    img.src ='manha.png'
+    document.body.style.background = 'rgb(145, 160, 147)'
+} else if (hora >= 12 && hora < 18) {
+    // Boa tarde!
+    img.src ='tarde.png'
+    document.body.style.background = 'rgb(184, 141, 96)'
+} else {
+    // Boa noite!
+    img.src ='noite.png'
+    document.body.style.background = 'rgb(40, 41, 46)';
+}
+}
