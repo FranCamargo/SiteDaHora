cdff905b6ac1b7a2b2c4d8206345ba6465a492f0
Fran Camargo <francamargoads@gmail.com>
2024-11-22 23:45:56 -0300

    enhancing hours and messages

--- sitedahora.js
+++ sitedahora.js
@@ -1,22 +1,30 @@
 function carregar() {
-  var msg = window.document.getElementById('msg');
-  var img = window.document.getElementById('imagem');
-  var data = new Date();
-  var hora = data.getHours();
+  let msg = window.document.getElementById("msg");
+  let img = window.document.getElementById("imagem");
+  let data = new Date();
+  let hora = data.getHours();
+  let minutos = data.getMinutes();
+  let dia = data.getDay();
+  let mes = data.getMonth();
+  let ano = data.getFullYear();
 
-  msg.innerHTML = `Agora são ${hora} horas.`;
+  msg.innerHTML = `Agora são ${hora}:${minutos} hrs. <br>
+  Hoje é dia ${dia}/${mes}/${ano}.`;
 
-  if ((hora >= 0) && (hora < 12)) {
+  if (hora >= 0 && hora < 12) {
     // Bom dia!
-    img.src ='manha.png'
-    document.body.style.background = 'rgb(145, 160, 147)'
-} else if (hora >= 12 && hora < 18) {
+    saudacao.innerHTML = "Bom dia!"
+    img.src = "manha.png";
+    document.body.style.background = "rgb(145, 160, 147)";
+  } else if (hora >= 12 && hora < 18) {
     // Boa tarde!
-    img.src ='tarde.png'
-    document.body.style.background = 'rgb(184, 141, 96)'
-} else {
+    saudacao.innerHTML = "Boa tarde!"
+    img.src = "tarde.png";
+    document.body.style.background = "rgb(184, 141, 96)";
+  } else {
     // Boa noite!
-    img.src ='noite.png'
-    document.body.style.background = 'rgb(40, 41, 46)';
-}
+    saudacao.innerHTML = "Boa noite!"
+    img.src = "noite.png";
+    document.body.style.background = "rgb(40, 41, 46)";
+  }
 }
